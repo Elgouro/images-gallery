2734aa43c50c21fb73473fd1d1c0924d9f077e8d
evasiallou <evasiallou20@gmail.com>
2023-03-14 18:10:06 +0000

    control change component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,22 @@
+import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Search from './components/Search';
 
+
 const App = () =>{
+  const [word,setword] =useState('');
+
+  const handleSearchSubmit = (e) =>{
+    e.preventDefault();
+    console.log(e.target[0].value);
+  }
+
+  
   return (
     <div>
       <Header title= "DIGITAL ANIMAL PARK" />
-      <Search />
+      <Search word={word} setword={setword} handleSubmit={handleSearchSubmit}/>
     </div>
   );
 }
